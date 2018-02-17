822f890a3a566244beca234b120ef605e1f3e39d
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2018-02-17 10:47:17 +0400

    some basic workings tested

---
+++ add.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "userbooks";
+$table = "books";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO $table (id, name)
+VALUES (null, 'TheRiver')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql2 = "SELECT id, name FROM books";
+$result = $conn->query($sql2);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
+?>
